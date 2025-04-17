337753f01ed65036b5d1821f13075cf0e8b42d05
Zaki Hussain <mail@zaki.me>
2025-04-17 03:24:00 +0100

    Made responsive for mobile and changed design a little

--- js/script.js
+++ js/script.js
@@ -1,34 +1,9 @@
-function change(hide, show, display) {
-    document.getElementById(hide).style.display="none";
-    document.getElementById(show).style.display=display;
-    document.getElementById(show).style.opacity="0";
-    setTimeout(visible, 10, show);
-}
-
-function visible(element) {
-    document.getElementById(element).style.opacity="1";
-}
-
-function projects() {
-    document.getElementById("right-container").style.maxWidth="30vw";
-    document.getElementById("home").style.opacity="0";
-    setTimeout(change, 500, "home", "projects", "flex");
-}
-
-function about() {
-    document.getElementById("right-container").style.maxWidth="30vw";
-    document.getElementById("home").style.opacity="0";
-    setTimeout(change, 500, "home", "about", "flex");
-}
-
-function aHome() {
-    document.getElementById("right-container").style.maxWidth="115.6px";
-    document.getElementById("about").style.opacity="0";
-    setTimeout(change, 500, "about", "home", "block");
-}
-
-function pHome() {
-    document.getElementById("right-container").style.maxWidth="115.6px";
-    document.getElementById("projects").style.opacity="0";
-    setTimeout(change, 500, "projects", "home", "block");
+function swapContent(id, flex) {
+    var content = document.getElementById("content");
+    content.style.flex = flex;
+    content.style.opacity = "0";
+    setTimeout(() => {
+        content.innerHTML = document.getElementById(id).innerHTML;
+        content.style.opacity = "1";
+    }, 500)
 }
